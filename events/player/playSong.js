267a35e8a06823ddfdbdc419c0a481cd0ca2440a
Rizky Pratama <104710750+Aruh1@users.noreply.github.com>
2023-07-17 09:58:13 +0000

    🐛 fix(bot.js): update event binding logic to handle player events separately 🐛 fix(events/player/playSong.js): fix condition to send queue message
    The event binding logic in `bot.js` has been updated to handle player events separately. If the `dirPath` ends with "player", the event will be bound to `discordClient.player` instead of `discordClient`. This change allows for better organization and separation of player-related events.
    
    In `events/player/playSong.js`, the condition to send the queue message has been fixed. Previously, the condition was not properly checking for the existence of `queue.textChannel`, resulting in potential errors. The condition has been updated to properly check for the existence of `queue` and `queue.textChannel` before sending the message.

--- events/player/playSong.js
+++ events/player/playSong.js
@@ -5,19 +5,12 @@ module.exports = async (client, queue, song) => {
     });
     lang = lang?.language || client.language;
     lang = require(`../../languages/${lang}.js`);
-    if (queue) {
-        if (!client.config.opt.loopMessage && queue?.repeatMode !== 0) return;
-        if (queue?.textChannel) {
-            queue?.textChannel
-                ?.send({
-                    content: lang.msg13
-                        .replace("{track?.title}", song?.name)
-                        .replace(
-                            "{queue?.connection.channel.name}",
-                            `<#${queue.voice.connection.joinConfig.channelId}>`
-                        )
-                })
-                .catch(e => {});
-        }
-    }
+    if (!queue && !client.config.opt.loopMessage && queue?.repeatMode !== 0 && !queue?.textChannel) return;
+    queue?.textChannel
+        ?.send({
+            content: lang.msg13
+                .replace("{track?.title}", song?.name)
+                .replace("{queue?.connection.channel.name}", `<#${queue.voice.connection.joinConfig.channelId}>`)
+        })
+        .catch(e => {});
 };
